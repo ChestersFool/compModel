3f47be91857ac786c8ae3b473d2ebf5e6e21c2aa
Kiiashchenko_Mykhailo <kiiashchenko.mykhailo@chnu.edu.ua>
2023-09-27 12:19:32 +0300

    lab1 array

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,15 +13,15 @@ public class Main {
 
         double from = 0;
         double k0;
-        ArrayList<Double> x = new ArrayList<>(20);
-        ArrayList<Double> y = new ArrayList<>(20);
-//        double[] x = new double[1000];
-//        double[] y = new double[1000];
+//        ArrayList<Double> x = new ArrayList<>(20);
+//        ArrayList<Double> y = new ArrayList<>(20);
+        double[] x = new double[1000];
+        double[] y = new double[1000];
         double[] h = new double[400];
-        x.add(1.);
-        y.add(1.);
-//        x[0] = 1.;
-//        y[0] = 1.;
+//        xArr.add(1.);
+//        yArr.add(1.);
+        x[0] = 1.;
+        y[0] = 1.;
 
         System.out.println("Enter step, k0: ");
         h[0] = sc.nextDouble();
@@ -54,8 +54,8 @@ public class Main {
         double[] xCorrect = calculateCorrectX(n, hOpt, from);
         double[] yCorrect = calculateCorrectY(n, hOpt, from);
 
-        System.out.println("x: " + x);
-        System.out.println("y: " + y);
+        System.out.println("x: " + Arrays.toString(x));
+        System.out.println("y: " + Arrays.toString(y));
         System.out.println("xCorrect: " + Arrays.toString(xCorrect));
         System.out.println("yCorrect: " + Arrays.toString(yCorrect));
         System.out.print("points: ");
@@ -64,16 +64,8 @@ public class Main {
         }
     }
 
-    private static void produceNRK21(double from, double k0, ArrayList<Double> x, ArrayList<Double> y, double[] h) {
+    private static void produceNRK21(double from, double k0, double[] x, double[] y, double[] h) {
         double pointCur = from;
-
-        double temp = x.get(0);
-        x.clear();
-        x.add(temp);
-
-        temp = y.get(0);
-        y.clear();
-        y.add(temp);
 
         // yn+1 = yn + h/2 * t2(t2 + t1)
         // t1 = f(tn+1, xn+1, yn+1)
@@ -81,8 +73,8 @@ public class Main {
         for (int i = 0; i < 3; i++) {
 //            System.out.println(h[i]);
             double pointNext = pointCur + h[i];
-            double xi1k = x.get(i);
-            double yi1k = y.get(i);
+            double xi1k = x[i];
+            double yi1k = y[i];
             double xi1k1 = 0; // init
             double yi1k1 = 0; // init
             int j = 0;
@@ -95,12 +87,12 @@ public class Main {
 
 //                System.out.println(t1 + " " + t2 + " " + l1 + " " + l2);
 
-                xi1k1 = x.get(i) + h[i] / 2 * t2 * (t1 + t2); // x1k+1 = ...
-                yi1k1 = y.get(i) + h[i] / 2 * l2 * (l1 + l2);
+                xi1k1 = x[i] + h[i] / 2 * t2 * (t1 + t2); // x1k+1 = ...
+                yi1k1 = y[i] + h[i] / 2 * l2 * (l1 + l2);
 
-//                System.out.println(j);
-//                System.out.println("xi1k1: " + xi1k1 + "; yi1k1: " + yi1k1);
-//                System.out.println("xi1k: " + xi1k + "; yi1k: " + yi1k);
+                System.out.println(j);
+                System.out.println("xi1k1: " + xi1k1 + "; yi1k1: " + yi1k1);
+                System.out.println("xi1k: " + xi1k + "; yi1k: " + yi1k);
 //                System.out.println("xi1k1c: " + xFuncCorrect(pointNext) + "; yi1k1c: " + yFuncCorrect(pointNext));
 
                 if (Math.abs(xi1k1 - xi1k) + Math.abs(yi1k1 - yi1k) <= eps) {
@@ -118,28 +110,28 @@ public class Main {
                 continue;
             }
 
-            x.add(xi1k1);
-            y.add(yi1k1);
-//            x[i + 1] = xi1k1;
-//            y[i + 1] = yi1k1;
+//            x.add(i+1,xi1k1 );
+//            y.add(i+1, yi1k1);
+            x[i + 1] = xi1k1;
+            y[i + 1] = yi1k1;
             h[i + 1] = h[i];
             pointCur = pointNext;
         }
     }
 
-    public static void produceJAN3(double from, ArrayList<Double> x, ArrayList<Double> y, double hOpt) {
+    public static void produceJAN3(double from, double[] x, double[] y, double hOpt) {
         double pointCur = from + 3 * hOpt;
         int n = (int) (1 / hOpt);
 
         for (int i = 3; i < n; i++) {
             double pointNext = pointCur + hOpt;
             // yn+3 = yn+1 + h/3 * (7 * f(xn+1, yn+1) - 2 * f(xn, yn) + f(xn-1, yn-1))
-            x.add(x.get(i - 2) + hOpt / 3 * (7 * xFuncDerivative(pointNext, x.get(i - 1), y.get(i - 1)) -
-                    2 * xFuncDerivative(pointCur, x.get(i - 2), y.get(i - 2)) +
-                    xFuncDerivative(pointCur, x.get(i - 3), y.get(i - 3))));
-            y.add(y.get(i - 2) + hOpt / 3 * (7 * yFuncDerivative(pointNext, x.get(i - 1), y.get(i - 1)) -
-                    2 * yFuncDerivative(pointCur, x.get(i - 2), y.get(i - 2)) +
-                    yFuncDerivative(pointCur, x.get(i - 3), y.get(i - 3))));
+            x[i] = x[i - 2] + hOpt / 3 * (7 * xFuncDerivative(pointNext, x[i - 1], y[i - 1]) -
+                    2 * xFuncDerivative(pointCur, x[i - 2], y[i - 2]) +
+                    xFuncDerivative(pointCur, x[i - 3], y[i - 3]));
+            y[i] = y[i - 2] + hOpt / 3 * (7 * yFuncDerivative(pointNext, x[i - 1], y[i - 1]) -
+                    2 * yFuncDerivative(pointCur, x[i - 2], y[i - 2]) +
+                    yFuncDerivative(pointCur, x[i - 3], y[i - 3]));
             pointCur = pointNext;
         }
     }
